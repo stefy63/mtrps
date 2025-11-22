e61bac0537be9e86cb4c056e2723caaa1941466c
Stefano <stefano.sca@gmail.com>
2025-11-22 11:37:15 +0100

    bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,7 +38,7 @@ class HomeController extends Controller
                 'movementsFrom',
                 'activeMaintenance'
             ])
-            ->orderByDesc('ente')
+            ->orderBy('ente')
             ->get();
 
         return view('home', compact('data'));
