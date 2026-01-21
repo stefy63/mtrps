35bf6dfdad40a7ac126e88ef608161cc3b9e04f0
Stefano Scarsella <stefano.sca@gmail.com>
2026-01-21 16:34:47 +0100

    Fix internet connection

--- app/Http/Controllers/AutoUpdateController.php
+++ app/Http/Controllers/AutoUpdateController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Services\AutoUpdateService;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Redirect;
 
 class AutoUpdateController extends Controller
 {
     public function __invoke(AutoUpdateService $service)
     {
+        if (!$this->isOnline()) {
+            return Redirect::back()
+                ->with('error', 'Impossibile connettersi al server. Verifica la tua connessione e riprova.');
+        }
         $response = response()->json($service->updateFromGit());
         if ($response->original['status'] === 'ok') {
             return Redirect::back()
@@ -16,5 +21,15 @@ class AutoUpdateController extends Controller
         }
         return Redirect::back()
             ->with('error', $response->original['message']);
+    }
+
+    public function isOnline()
+    {
+        try {
+            $response = Http::timeout(3)->get('https://www.google.com');
+            return $response->successful();
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
